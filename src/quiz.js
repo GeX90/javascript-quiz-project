6254725afd6e8bc5fc7cde1977a9ea3ad905d942
Jorge Jiménez <gexgt90@gmail.com>
2025-11-17 17:49:17 +0100

    Project Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,31 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
 
-    // 2. getQuestion()
+    moveToNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+        this.currentQuestionIndex += 1; 
+        return true; 
+    } else {
+        return false;
+    }
+}
+shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+    }
+}
+checkAnswer() {
     
-    // 3. moveToNextQuestion()
+}
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
 }
